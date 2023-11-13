f40768ae4cc9f817083ab1873ea4af7a4ef53136
Marko Leppänen <anterolep@gmail.com>
2023-11-13 22:20:05 +0200

    logging

--- src/apis/rio.ts
+++ src/apis/rio.ts
@@ -16,7 +16,7 @@ export const getCurrentScores = async (
   character: string,
   region: string = 'eu'
 ) => {
-  logger.info({ msg: 'getting rating', realm, character })
+  logger.info({ msg: 'getting rating', realm, character, url: await scoreUrl(realm.toLowerCase(), character.toLowerCase(), region) })
   const scores: CurrentRatings = await (
     await fetch(
       await scoreUrl(realm.toLowerCase(), character.toLowerCase(), region),
