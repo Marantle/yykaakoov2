f67e6083598b63ed76063cc8e86d1c4305cdfa91
Marko Leppänen <anterolep@gmail.com>
2023-11-13 22:28:52 +0200

    ephemereal off

--- src/commands/followed/followedrating.ts
+++ src/commands/followed/followedrating.ts
@@ -48,49 +48,12 @@ const followedrating: Command = {
       }`
     })
 
-    const embed = buildEmbedFromStringArray(ar)
+    const embed = buildEmbedFromStringArray(ar, 'Your followed characters m+ ratings')
 
     return await interaction.reply({
       embeds: [embed],
-      ephemeral:
-        !interaction.options.getString('realm') &&
-        !interaction.options.getString('character'),
     })
   },
-}
-
-function buildRioEmbed(
-  mytchicPlusProgress: CurrentScores,
-  character: string,
-  thumbnail: string
-) {
-  const topKeys = mytchicPlusProgress.mythic_plus_scores_by_season[0].scores
-  const msg =
-    '```' +
-    `
-All:    ${topKeys.all}
-DPS:    ${topKeys.dps > 0 ? topKeys.dps : 'N/A'}
-Healer: ${topKeys.healer > 0 ? topKeys.healer : 'N/A'}
-Tank:   ${topKeys.tank > 0 ? topKeys.tank : 'N/A'}
-Spec 1: ${topKeys.spec_0 > 0 ? topKeys.spec_0 : 'N/A'}
-Spec 2: ${topKeys.spec_1 > 0 ? topKeys.spec_1 : 'N/A'}
-Spec 3: ${topKeys.spec_2 > 0 ? topKeys.spec_2 : 'N/A'}
-Spec 4: ${topKeys.spec_3 > 0 ? topKeys.spec_3 : 'N/A'}
-` +
-    '```'
-  const embed = new EmbedBuilder()
-    .setColor(0x0099ff)
-    .setTitle(
-      'Rio for ' + character.charAt(0).toUpperCase() + character.slice(1)
-    )
-    .setThumbnail(thumbnail)
-    .addFields({ name: ' ', value: msg })
-    .addFields({
-      name: ' ',
-      value: `[Raider.io profile](${mytchicPlusProgress.profile_url})`,
-    })
-
-  return embed
 }
 
 export default followedrating
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -3,11 +3,11 @@ import { EmbedBuilder } from 'discord.js'
 export const c = (s?: string) =>
   s ? s.charAt(0).toUpperCase() + s.slice(1) : 'error'
 
-export function buildEmbedFromStringArray(ar: string[]) {
+export function buildEmbedFromStringArray(ar: string[], title?: string) {
   const msg = '```' + ar.join('\n') + '```'
   const embed = new EmbedBuilder()
     .setColor(0x0099ff)
-    .setTitle(' ')
+    .setTitle(title ?? ' ')
     .addFields({ name: ' ', value: msg })
   return embed
 }
